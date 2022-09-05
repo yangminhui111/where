54ac2528b4b39be59ef0d880e699d09d35d26ef7
unknown <1497972988@qq.com>
2022-09-05 16:39:41 +0800

    小图标

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,127 @@
+<template>
+    <div class="icons">
+        <swiper>
+            <swiper-slide v-for="(page, index) of pages" :key="index">
+                <div class="icon" v-for="item of page" :key="item.id">
+                    <div class="icon-img">
+                        <img class="icon-img-content" :src="item.imgUrl" alt="" />
+                    </div>
+                    <p class="icon-desc">{{ item.desc }}</p>
+                </div>
+            </swiper-slide>
+        </swiper>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'HomeIcons',
+        data(){
+            return {
+                list:[
+                    {
+                        id:'0001',
+                        imgUrl:'https://s.qunarzz.com/homenode/images/touchheader/hotel.png',
+                        content:'酒店'
+                    },
+                    {
+                        id:'0002',
+                        imgUrl:'https://s.qunarzz.com/homenode/images/touchheader/flight.png',
+                        content:'机票'
+                    },
+                    {
+                        id:'0003',
+                        imgUrl:'https://s.qunarzz.com/homenode/images/touchheader/train.png',
+                        content:'火车票'
+                    },
+                    {
+                        id:'0004',
+                        imgUrl:'https://s.qunarzz.com/homenode/images/touchheader/package.png',
+                        content:'度假'
+                    },
+                    {
+                        id:'0005',
+                        imgUrl:'https://s.qunarzz.com/homenode/images/touchheader/piao.png',
+                        content:'景点门票'
+                    },
+                    {
+                        id:'0006',
+                        imgUrl:'https://s.qunarzz.com/homenode/images/touchheader/piao.png',
+                        content:'景点门票'
+                    },
+                    {
+                        id:'0007',
+                        imgUrl:'https://s.qunarzz.com/homenode/images/touchheader/piao.png',
+                        content:'景点门票'
+                    },
+                    {
+                        id:'0008',
+                        imgUrl:'https://s.qunarzz.com/homenode/images/touchheader/piao.png',
+                        content:'景点门票'
+                    }
+                ]
+            }
+        },
+        computed:{
+            // 分页，像焦点轮播图一样翻页
+            pages(){
+                const pages=[];
+                this.list.forEach((item,index)=>{
+                    const page=Math.floor(index/8)
+                    // 如果没有子元素就创建一个空数组
+                    if(!pages[page]){
+                        pages[page]=[]
+                    }
+                    // 如果有就添加，数组长度等于8就重新创建一个数组
+                    pages[page].push(item)
+                })
+                return pages
+            }
+        }
+    }
+</script>
+
+<style lang="less" scoped>
+@import '@/assets/styles/varibles.less';
+
+.icons {
+    margin-top: .1rem;
+}
+
+.icon {
+    float: left;
+    width: 25%;
+    height: 0;
+    padding-bottom: 25%;
+    position: relative;
+}
+
+.icon-img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: .44rem;
+    box-sizing: border-box;
+    padding: .1rem;
+}
+
+.icon-img-content {
+    height: 100%;
+    display: block;
+    margin: 0 auto;
+}
+
+.icon-desc {
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    height: .44rem;
+    line-height: .44rem;
+    text-align: center;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+}
+</style>
