ae7a6d03848237879245fe4c66ac51272b925458
unknown <1497972988@qq.com>
2022-09-05 21:35:07 +0800

    周末游

--- src/pages/home/components/Weekend.vue
+++ src/pages/home/components/Weekend.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="title">
-            热销推荐
+            周末去哪
         </div>
         <ul>
             <li class="item border-bottom" v-for="item in recommendList" :key="item.id">
@@ -11,7 +11,6 @@
                 <div class="item-info">
                     <p class="item-title">{{item.title}}</p>
                     <p class="item-desc">{{item.desc}}</p>
-                    <button class="item-button">查看详情</button>
                 </div>
             </li>
         </ul>
@@ -20,13 +19,13 @@
 
 <script>
     export default {
-        name:'HomeRecommend',
+        name:'HomeWeekend',
         data(){
             return {
                 recommendList:[
                 {
                     id:'0001',
-                    imgUrl:'http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_600x330_bf9c4904.jpg',
+                    imgUrl:'http://img1.qunarzz.com/piao/fusion/1802/51/e78f936a5b404102.jpg_640x200_c14f0b3a.jpg',
                     title:'大连圣亚海洋世界',
                     desc:'浪漫大连首站，浪漫的海洋主题公园'
                 },
@@ -55,18 +54,15 @@
     background: #eee;
     text-indent: .2rem;
 }
-.item{
+.item-img-wrapper{
     overflow: hidden;
-    display: flex;
-    height: 1.9rem;
+    height: 0;
+    padding-bottom: 33.9%;
 }
 .item-img{
-    width: 1.7rem;
-    height: 1.7rem;
-    padding: .1rem;
+    width: 100%;
 }
 .item-info{
-    flex: 1;
     padding: .1rem;
 }
 .item-title{
@@ -77,13 +73,5 @@
 .item-desc{
     line-height: .4rem;
     color: #ccc;
-}
-.item-button{
-    line-height: .44rem;
-    margin-top: .16rem;
-    background: #ff9300;
-    padding: .1rem .2rem;
-    border-radius: .06rem;
-    color: #fff;
 }
 </style>
