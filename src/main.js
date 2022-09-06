2e881f82bba965f7eaf51354359eb4c6dc70d77f
unknown <1497972988@qq.com>
2022-09-06 19:52:45 +0800

    city-ajax

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
+import fastClick from "fastclick";
 import 'swiper/dist/css/swiper.css'
 import './assets/styles/reset.css'
 import './assets/styles/iconfont.css'
@@ -7,6 +8,7 @@ import router from './router/index'
 import App from './App.vue'
 Vue.config.productionTip = false
 Vue.use(VueAwesomeSwiper)
+fastClick.attach(document.body);
 new Vue({
   router,
   // store,
--- src/pages/city/CityView.vue
+++ src/pages/city/CityView.vue
@@ -2,17 +2,48 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
+        <city-list :hotCities="hotCities" :cities="cities"></city-list>
+        <city-alphabet :cities="cities"></city-alphabet>
     </div>
 </template>
 
 <script>
     import CityHeader from './components/Header.vue'
     import CitySearch from './components/Search.vue'
+    import CityList from './components/List.vue'
+    import CityAlphabet from './components/Alphabet.vue'
+import axios from 'axios'
     export default {
         name:'CityView',
+        data(){
+            return {
+                cities:[],
+                hotCities:[]
+            }
+        },
         components:{
             CityHeader,
-            CitySearch
+            CitySearch,
+            CityList,
+            CityAlphabet
+        },
+        methods:{
+            getCityInfo(){
+                axios.get('/static/mock/city.json')
+                .then(this.getCityInfoSucc)
+            },
+            getCityInfoSucc(res){
+                console.log(res)
+                res=res.data;
+                if(res.ret&&res.data){
+                    const data=res.data;
+                    this.cities=data.cities;
+                    this.hotCities=data.hotCities
+                }
+            }
+        },
+        mounted(){
+            this.getCityInfo()
         }
     }
 </script>
